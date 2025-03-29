f17ee85dea01698b5ae80742552f844fc825e52f
Rinat Mustafin <rin.mustafin2010@yandex.ru>
2025-03-29 19:31:41 +0500

    Добавить аннотацию @fieldefaults и убрать @notempty

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,24 +1,26 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import lombok.experimental.FieldDefaults;
 import java.time.LocalDate;
 
 /**
  * Film.
  */
+@FieldDefaults (level = AccessLevel.PRIVATE)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Film {
-    private Long id;
-    @NotEmpty
-    private String name;
+    Long id;
+    @NotBlank
+    String name;
     @Size(max = 200)
-    private String description;
-    private LocalDate releaseDate;
+    String description;
+    LocalDate releaseDate;
     @Positive
-    private int duration;
+    int duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,24 +1,25 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
 
-
+@FieldDefaults (level = AccessLevel.PRIVATE)
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class User {
-    private Long id;
-    private String name;
-    @NotEmpty(message = "Логин не должен быть null или пуст")
+    Long id;
+    String name;
+    @NotBlank(message = "Логин не должен быть null или пуст")
     @Pattern(regexp = "\\S+", message = "Логин не должен содержать пробелов")
-    private String login;
+    String login;
     @Email
-    @NotEmpty
-    private String email;
-    private LocalDate birthday;
+    @NotBlank
+    String email;
+    LocalDate birthday;
 }
